23ac583bca6195ae82283a8190cda32f78100346
Roger Qiu <roger@formbay.com.au>
2019-02-07 15:05:41 +1100

    Directory capability and fixed package set

--- pkgs.nix
+++ pkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/650d8c93a16333e9390db0269327bd2d47f02fc7.tar.gz) {}
+import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/8008d0bbc691067b5a5f6f5e916a53b53925bf51.tar.gz) {}
