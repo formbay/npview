5e7e1fa7eb66f69b178dd8dbfc7d484de355a39d
Roger Qiu <roger@formbay.com.au>
2021-06-09 12:50:31 +1000

    Pinning to a76d51dc52ed08f16f9747c4685ec78c1b9a5c4c latest 21.05

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (
-  let rev = "7e9b0dff974c89e070da1ad85713ff3c20b0ca97"; in
+  let rev = "a76d51dc52ed08f16f9747c4685ec78c1b9a5c4c"; in
   fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz"
 )
